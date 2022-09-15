11b7e35949ee6431f529cd1a9b4e99031fc1f418
JinglunMa24 <2461447099ma@gmail.com>
2022-09-15 12:03:00 -0400

    added

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 // add your code as appropriate 
 #include <iostream>
+#include "reservoir.h"
 
-int main()
-{
+int main() {
+	
+	//test case for TASK A
+	std::cout << "East basin storage: " << get_east_storage("05/20/2018") << " billion gallons" << std::endl;
+	std::cout << "East basin storage: " << get_east_storage("09/30/2018") << " billion gallons" << std::endl;
   
+	//test case for TASK B
+	std::cout << "Minimum storage in East basin: " << get_min_east() << " billion gallons" << std::endl;
+	std::cout << "Maximum storage in East basin: " << get_max_east() << " billion gallons" << std::endl;
 
-  
-  return 0;
+	return 0;
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -1,1 +1,106 @@
 // add your code
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <climits>
+#include "reservoir.h"
+
+double get_east_storage(std::string input) {
+	std::ifstream fin("Current_Reservoir_Levels.tsv");
+    if (fin.fail()) {
+        std::cerr << "File cannot be opened for reading." << std::endl;
+        exit(1); // exit if failed to open the file
+    }
+
+    std::string junk, date;
+    getline(fin, junk);
+
+    double eastSt, eastEl, westSt, westEl, output;
+
+    while(fin >> date >> eastSt >> eastEl >> westSt >> westEl) { 
+        // this loop reads the file line-by-line
+        // extracting 5 values on each iteration 
+    
+        fin.ignore(INT_MAX, '\n'); //skips to the end of line, 
+                              //ignorring the remaining columns 
+
+        if (input.compare(date) == 0) {
+            output = eastSt;
+        }
+    }
+
+    fin.close();
+
+    return output;
+}
+
+
+double get_min_east() {
+    std::ifstream fin("Current_Reservoir_Levels.tsv");
+    if (fin.fail()) {
+        std::cerr << "File cannot be opened for reading." << std::endl;
+        exit(1); // exit if failed to open the file
+    }
+
+    std::string junk, date;
+    getline(fin, junk);
+
+    double eastSt, eastEl, westSt, westEl, output;
+    bool min = true;
+
+    while(fin >> date >> eastSt >> eastEl >> westSt >> westEl) { 
+        // this loop reads the file line-by-line
+        // extracting 5 values on each iteration 
+    
+        fin.ignore(INT_MAX, '\n'); //skips to the end of line, ignorring the remaining columns 
+
+        if (min) {
+            output = eastSt;
+            min = false;
+        }
+
+        if (output > eastSt&& !min) {
+            output = eastSt;
+        }
+    
+    }
+
+    fin.close();
+
+    return output;
+}
+
+
+double get_max_east() {
+    std::ifstream fin("Current_Reservoir_Levels.tsv");
+    if (fin.fail()) {
+        std::cerr << "File cannot be opened for reading." << std::endl;
+        exit(1); // exit if failed to open the file
+    }
+
+    std::string junk, date;
+    getline(fin, junk);
+
+    double eastSt, eastEl, westSt, westEl, output;
+    bool max = true;
+
+    while(fin >> date >> eastSt >> eastEl >> westSt >> westEl) { 
+        // this loop reads the file line-by-line
+        // extracting 5 values on each iteration 
+    
+        fin.ignore(INT_MAX, '\n'); //skips to the end of line, ignorring the remaining columns 
+
+        if (max) {
+            output = eastSt;
+            max = false;
+        }
+        
+        if (output < eastSt && !max) {
+            output = eastSt;
+        }
+    }
+
+    fin.close();
+
+    return output;
+}
